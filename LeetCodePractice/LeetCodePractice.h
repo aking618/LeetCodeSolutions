cca1509803f007dc78f7c840473f69c458e2d46c
aking618 <ashsquads@gmail.com>
2023-09-23 18:52:38 -0500

    Initial Commit

---
+++ LeetCodePractice/LeetCodePractice.h
@@ -0,0 +1,18 @@
+//
+//  LeetCodePractice.h
+//  LeetCodePractice
+//
+//  Created by Ayren King on 9/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeetCodePractice.
+FOUNDATION_EXPORT double LeetCodePracticeVersionNumber;
+
+//! Project version string for LeetCodePractice.
+FOUNDATION_EXPORT const unsigned char LeetCodePracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeetCodePractice/PublicHeader.h>
+
+
